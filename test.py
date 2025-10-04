5bcae55cc45d6d5a0fde71ff74cfb94b55828092
wata <watanabe.akinori.1224@gmail.com>
2025-10-04 20:47:34 +0900

    pyvesc

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import time
 import serial
-from pyvesc.setters import SetDutyCycle
-from pyvesc import encode
+import pyvesc
 
 # UART設定
 PORT = "/dev/serial0"
@@ -10,17 +9,15 @@ ser = serial.Serial(PORT, BAUDRATE, timeout=0.1)
 
 def set_duty(duty):
     duty = max(-1.0, min(1.0, duty))
-    msg = SetDutyCycle(duty)
-    ser.write(encode(msg))
+    msg = pyvesc.SetDutyCycle(duty)
+    ser.write(pyvesc.encode(msg))
 
 try:
     while True:
-        # 徐々に上げる
         for d in [i/20 for i in range(21)]:
             set_duty(d)
             print(f"Duty: {d:.2f}")
             time.sleep(0.2)
-        # 徐々に下げる
         for d in [i/20 for i in range(20, -1, -1)]:
             set_duty(d)
             print(f"Duty: {d:.2f}")
