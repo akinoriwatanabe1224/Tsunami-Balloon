6b7e70c6e83ab1ead226eb7546fa18f138efaad7
wata <watanabe.akinori.1224@gmail.com>
2025-10-04 20:58:12 +0900

    pyvesc-fix

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
 import time
 import serial
-from pyvesc.messages.setters import SetDutyCycle  # pyvesc 1.0.5 構造
+from pyvesc.messages.setters import SetDutyCycle
 from pyvesc.interface import encode
 
 PORT = "/dev/serial0"
 BAUDRATE = 115200
-
 ser = serial.Serial(PORT, BAUDRATE, timeout=0.1)
 
 def set_duty(duty: float):
     duty = max(-1.0, min(1.0, duty))
-    msg = SetDutyCycle(duty)
+    duty_int = int(duty * 1000)  # 0～1000 に変換
+    msg = SetDutyCycle(duty_int)
     ser.write(encode(msg))
 
 try:
