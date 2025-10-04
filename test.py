289a3a0d49de47a5e24ed96dc88289624c831cfb
wata <watanabe.akinori.1224@gmail.com>
2025-10-04 20:52:10 +0900

    pyvesc

--- test.py
+++ test.py
@@ -2,17 +2,19 @@ import time
 import serial
 import pyvesc
 
-# UART設定
 PORT = "/dev/serial0"
 BAUDRATE = 115200
 ser = serial.Serial(PORT, BAUDRATE, timeout=0.1)
 
 def set_duty(duty):
     duty = max(-1.0, min(1.0, duty))
+    # SetDutyCycle は float を受け取る
     msg = pyvesc.SetDutyCycle(duty)
-    ser.write(pyvesc.encode(msg))
+    packet = pyvesc.encode(msg)
+    ser.write(packet)
 
 try:
+    print("Starting motor ramp test...")
     while True:
         for d in [i/20 for i in range(21)]:
             set_duty(d)
