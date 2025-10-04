dd87f24408a6e45eea9d12c5e84facd2824ab96b
wata <watanabe.akinori.1224@gmail.com>
2025-10-04 21:25:17 +0900

    pyvesc-fix

--- test.py
+++ test.py
@@ -8,19 +8,19 @@ BAUDRATE = 115200
 ser = serial.Serial(PORT, BAUDRATE, timeout=0.1)
 
 def set_duty(duty: float):
-    duty = max(-1.0, min(1.0, duty))
+    duty = max(-10.0, min(10.0, duty))
     duty_int = int(duty * 1000)  # 0～1000 に変換
     msg = SetDutyCycle(duty_int)
     ser.write(encode(msg))
 
 try:
     while True:
-        for d in [i/20 for i*10 in range(21)]:
-            set_duty(d)
+        for d in [i/20 for i0 in range(21)]:
+            set_duty(d*10)
             print(d)
             time.sleep(0.2)
-        for d in [i/20 for i*10 in range(20,-1,-1)]:
-            set_duty(d)
+        for d in [i/20 for i in range(20,-1,-1)]:
+            set_duty(d*10)
             print(d)
             time.sleep(0.2)
 except KeyboardInterrupt:
