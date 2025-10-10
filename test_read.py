ce755aed84ec6cc5ca80bb8984be1e5f154e4eb3
wata <watanabe.akinori.1224@gmail.com>
2025-10-11 03:51:52 +0900

    add csv

--- test_read.py
+++ test_read.py
@@ -1,37 +1,17 @@
-import serial
-import time
+from pyvesc.messages.getters import GetValues
 from pyvesc.protocol.interface import encode_request, decode
+import serial
 
-PORT = "/dev/serial0"
-BAUD = 115200
+# シリアル
+ser = serial.Serial("/dev/serial0", 115200, timeout=0.5)
 
-ser = serial.Serial(PORT, BAUD, timeout=0.5)
+# GET_VALUES メッセージ作成
+msg = GetValues()  # クラスのインスタンス
+pkt = encode_request(msg)
 
-COMM_GET_VALUES = 4  # VESC GET_VALUES
+# 送信
+ser.write(pkt)
 
-try:
-    while True:
-        # GET_VALUESリクエストを作成
-        pkt = encode_request(COMM_GET_VALUES)
-        ser.write(pkt)
-        time.sleep(0.05)
-
-        # VESCから応答を受信
-        data = ser.read(1024)
-        if data:
-            msg, _ = decode(data)
-            if msg:
-                print({
-                    'temp_fet': msg.temp_fet / 10,
-                    'temp_motor': msg.temp_motor / 10,
-                    'current_motor': msg.current_motor / 100,
-                    'current_in': msg.current_in / 100,
-                    'duty': msg.duty / 1000
-                })
-        else:
-            print("no reply")
-        time.sleep(0.5)
-
-except KeyboardInterrupt:
-    ser.close()
-    print("Stopped.")
+# 受信
+data = ser.read(1024)
+print(data)
