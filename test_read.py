4377e2f8874b6ab1f4119b0ff0337a61079b53ae
wata <watanabe.akinori.1224@gmail.com>
2025-10-11 02:39:49 +0900

    add csv

--- test_read.py
+++ test_read.py
@@ -10,15 +10,15 @@ ser = serial.Serial(PORT, BAUDRATE, timeout=0.1)
 
 try:
     while True:
-        # 値リクエストを送信
-        ser.write(encode(GetValues()))
-        
+        # 値リクエストを送信（クラスを渡す点に注意！）
+        ser.write(encode(GetValues))
+
         # 受信バッファからデコード
-        data = ser.read(100)  # 100バイト程度読む
+        data = ser.read(256)  # 256バイトぐらい読む
         if data:
             for msg in decode(data):
-                print(msg)  # VESCから返ってきたステータス
-        
+                print(msg)  # VESCから返ってきたステータスオブジェクト
+
         time.sleep(0.5)
 
 except KeyboardInterrupt:
