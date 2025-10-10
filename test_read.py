496161cccb16a025a15fbe7c02500feeaae4029f
wata <watanabe.akinori.1224@gmail.com>
2025-10-11 03:46:42 +0900

    add csv

--- test_read.py
+++ test_read.py
@@ -1,22 +1,60 @@
-# read_vesc_values.py
 import serial
-from pyvesc import get_values
+import time
+import pyvesc
+from pyvesc import VESCMessage, encode, decode
 
+# VESCのUARTポートとボーレートを設定
 PORT = "/dev/serial0"
 BAUD = 115200
 
+# シリアルポートの初期化
 ser = serial.Serial(PORT, BAUD, timeout=0.5)
 
+# GET_VALUESメッセージの定義
+class GetValues(VESCMessage):
+    id = 4
+    fields = [
+        ('temp_fet', 'h'),
+        ('temp_motor', 'h'),
+        ('current_motor', 'i'),
+        ('current_in', 'i'),
+        ('duty', 'h'),
+        ('rpm', 'i'),
+        ('amp_hours', 'f'),
+        ('amp_hours_charged', 'f'),
+        ('watt_hours', 'f'),
+        ('watt_hours_charged', 'f'),
+        ('tachometer', 'i'),
+        ('tachometer_abs', 'i'),
+        ('fault_code', 'B')
+    ]
+
+# メインループ
 try:
     while True:
-        values = get_values(ser)  # VESCから生データを取得してパース
-        print({
-            'temp_fet': values.temp_fet,       # °C
-            'temp_motor': values.temp_motor,   # °C
-            'current_motor': values.current_motor,  # A
-            'current_in': values.current,           # A
-            'duty': values.duty
-        })
+        # GET_VALUESメッセージをエンコードして送信
+        msg = GetValues()
+        packet = encode(msg)
+        ser.write(packet)
+        time.sleep(0.05)
+
+        # VESCからの応答を読み取り
+        data = ser.read(1024)
+        if data:
+            # データをデコードして解析
+            msg, _ = decode(data)
+            if msg:
+                print({
+                    'temp_fet': msg.temp_fet / 10,
+                    'temp_motor': msg.temp_motor / 10,
+                    'current_motor': msg.current_motor / 100,
+                    'current_in': msg.current_in / 100,
+                    'duty': msg.duty / 1000
+                })
+        else:
+            print("No data received")
+        time.sleep(0.5)
+
 except KeyboardInterrupt:
     ser.close()
-    print("Stopped")
+    print("Stopped.")
