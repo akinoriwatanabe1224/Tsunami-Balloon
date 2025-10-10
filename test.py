8416994a3a210499aeeffe2e257db71136028536
wata <watanabe.akinori.1224@gmail.com>
2025-10-11 00:02:32 +0900

    add csv

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ USE_ABSOLUTE_TIME = True         # True: 時刻, False: 経過時間[s]
 # =============================
 # 初期化
 # =============================
-vesc = VESC(port=PORT, baudrate=BAUDRATE=BAUDRATE)
+vesc = VESC(port=PORT, baudrate=BAUDRATE)
 os.makedirs(SAVE_DIR, exist_ok=True)
 
 # CSVファイル名に日付を付ける
