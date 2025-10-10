ce8e7835cea184580dfee5eb4375882173688599
wata <watanabe.akinori.1224@gmail.com>
2025-10-11 01:39:15 +0900

    add csv

--- test_read.py
+++ test_read.py
@@ -1,33 +1,5 @@
-import sys
-import time
+import serial
 
-# PyVESC の親フォルダを追加
-sys.path.append('/home/pi/Tsunami-Balloon/PyVESC')
-
-# VESC クラスをインポート
-from pyvesc.VESC.VESC import VESC
-
-# =============================
-# UART 設定
-# =============================
-PORT = '/dev/serial0'  # UART ポート
-BAUDRATE = 115200       # ボーレート
-
-# VESC オブジェクト作成
-vesc = VESC(serial_port=PORT, baudrate=BAUDRATE)
-
-# =============================
-# メインループ
-# =============================
-try:
-    while True:
-        measurements = vesc.get_measurements()
-        print(
-            f"ERPM: {measurements.rpm:.1f}, "
-            f"Motor Current: {measurements.current_motor:.2f} A, "
-            f"Input Voltage: {measurements.v_in:.2f} V"
-        )
-        time.sleep(0.5)
-
-except KeyboardInterrupt:
-    print("Program stopped")
+ser = serial.Serial('/dev/serial0', 115200, timeout=0.5)
+ser.write(b'\x02')  # 適当な1バイト送信
+print(ser.read(10))
