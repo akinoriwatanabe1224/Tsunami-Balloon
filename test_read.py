e86c2afd3c58efb597b0c0d99370685f19a457a7
wata <watanabe.akinori.1224@gmail.com>
2025-10-11 01:48:59 +0900

    add csv

--- test_read.py
+++ test_read.py
@@ -1,5 +1,15 @@
-import serial
+# test_read.py
+import time
+from pyvesc.VESC import VESC    # pipで入れたpyvescならこれでOK
 
-ser = serial.Serial('/dev/serial0', 115200, timeout=0.5)
-ser.write(b'\x02')  # 適当な1バイト送信
-print(ser.read(10))
+PORT = "/dev/serial0"  # あるいは /dev/ttyAMA0, /dev/ttyS0 等
+BAUDRATE = 115200
+
+with VESC(serial_port=PORT, baudrate=BAUDRATE) as vesc:
+    try:
+        while True:
+            measurements = vesc.get_measurements()
+            print(f"ERPM: {measurements.rpm:.1f}, Motor Current: {measurements.current_motor:.2f} A, Voltage: {measurements.v_in:.2f} V")
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        print("Stopped")
