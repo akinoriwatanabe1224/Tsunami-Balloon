f1661388d862f1d7bca91901f6b1a0b7f28052f0
wata <watanabe.akinori.1224@gmail.com>
2025-12-18 07:10:19 +0900

    relay

--- test/relay.py
+++ test/relay.py
@@ -3,19 +3,23 @@ import time
 
 PIN = 17
 
-def callback(channel):
-    if GPIO.input(PIN):
-        print("CONNECTED")
-    else:
-        print("DISCONNECTED")
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.add_event_detect(PIN, GPIO.BOTH, callback=callback, bouncetime=50)
+print("waiting for edge...")
 
 try:
     while True:
-        time.sleep(1)
+        channel = GPIO.wait_for_edge(PIN, GPIO.BOTH)
+        if channel is None:
+            continue
+
+        if GPIO.input(PIN):
+            print("CONNECTED")
+        else:
+            print("DISCONNECTED")
+
 except KeyboardInterrupt:
+    pass
+finally:
     GPIO.cleanup()
