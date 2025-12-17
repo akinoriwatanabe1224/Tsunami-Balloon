73e47caef56fe03fe92f2616f36b3d309bb9d7bb
wata <watanabe.akinori.1224@gmail.com>
2025-12-18 08:14:36 +0900

    main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 import threading
 from src.duty_forward_revers import VESCDutyController
 from src.relay import RelayController
+from vesc.reader import VESCReader
 
 # ===== 設定 =====
 SERIAL_PORT = "/dev/serial0"
@@ -13,6 +14,11 @@ MAX_DUTY = 10
 STEP_DELAY = 0.05
 RUN_TIME_SEC = 5        # モータ回転時間
 COOLDOWN_SEC = 4        # 再トリガ無視時間
+
+LOG_INTERVAL = 0.05     # readerの取得周期
+CSV_ENABLE = True
+CSV_FILE = "log/0g.csv"
+CSV_FIELDS = ["time", "duty", "rpm"]
 # =================
 
 busy_lock = threading.Lock()
@@ -39,18 +45,29 @@ def with_lock(func):
                 print("READY")
 
             threading.Thread(target=release, daemon=True).start()
-
     return wrapper
 
 
 def main():
     ser = serial.Serial(SERIAL_PORT, BAUDRATE, timeout=0.1)
+
+    # --- Reader（ログ・表示） ---
+    reader = VESCReader(
+        ser,
+        interval=LOG_INTERVAL,
+        csv_enable=CSV_ENABLE,
+        csv_filename=CSV_FILE,
+        csv_fields=CSV_FIELDS
+    )
+
+    # --- Duty制御 ---
     duty = VESCDutyController(
         ser,
         max_duty=MAX_DUTY,
         step_delay=STEP_DELAY
     )
 
+    # --- GPIO ---
     relay = RelayController(pin_forward=17, pin_reverse=27)
 
     @with_lock
@@ -74,9 +91,11 @@ def main():
 
     try:
         print("SYSTEM READY")
+        reader.start()        # ← ログ開始
         relay.wait()
     finally:
         print("SYSTEM STOP")
+        reader.stop()
         duty.emergency_stop()
         ser.close()
 
